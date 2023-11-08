8a3777211815828d3bc9f7047097f92801a3c32e
joaquinxtomas <jtppalacios@gmail.com>
2023-11-08 15:16:21 -0300

    first_commit

---
+++ lectura.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/xuri/excelize/v2"
+)
+
+func main() {
+	f, err := excelize.OpenFile("prueba.xlsx")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer func() {
+		// Close the spreadsheet.
+		if err := f.Close(); err != nil {
+			fmt.Println(err)
+		}
+	}()
+	// Get value from cell by given worksheet name and cell reference.
+	cell, err := f.GetCellValue("Sheet0", "B2")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(cell)
+	// Get all the rows in the Sheet1.
+	rows, err := f.GetRows("Sheet0")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	for _, row := range rows {
+		for _, colCell := range row {
+			fmt.Println(colCell, "\t")
+		}
+		fmt.Println()
+	}
+}
